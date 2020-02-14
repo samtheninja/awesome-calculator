cb6b965ba0e835c13ef62657042150efce58f01a
Samuel-Mwaura <mr.s.n.mwaura@gmail.com>
2020-02-14 12:13:35 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var add=function(number1,number2) {
+  return number1 + number2;
+  };
+  var number1=parseInt(prompt("addition enter first number"))
+  var number2=parseInt(prompt("addition enter second number"))
+  alert(add(number1,number2));
+  var subtract=function(number1, number2) {
+    return number1 - number2;
+  };
+  var number1=parseInt(prompt("subtraction enter first number"))
+  var number2=parseInt(prompt("subtraction enter second number"))
+  alert(subtract(number1,number2));
+  var multiply=function(number1,number2) {
+  return number1*number2;
+  };
+  var number1=parseInt(prompt("multiply enter first number"))
+  var number2=parseInt(prompt("multiply enter second number"))
+  alert(multiply(number1,number2));
+  var divide=function(number1, number2) {
+    return number1/number2;
+  };
+  var number1=parseInt(prompt("divide enter first number"))
+  var number2=parseInt(prompt("divide enter second number"))
+  alert(divide(number1,number2));
+  
+  
+  
+
+
